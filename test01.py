8b212522a59de9e812543f856a4998f66e8eba72
shihaibin-art <shihaibin_vip@163.com>
2020-09-18 15:49:28 +0800

    stash

--- test01.py
+++ test01.py
@@ -1,16 +1,16 @@
 """
-    读取文件
+    方案一
 """
 file = open('./dict.txt')
 def func(target):
-    for line in file:
-        end = line.find(" ")
-        if line[0:end] == target:
-            file.close()
-            return line
-        if target > line[0:end]:
-            file.close()
-            return None
-
+    # for line in file:
+    #     end = line.find(" ")
+    #     if line[0:end] == target:
+    #         file.close()
+    #         return line
+    #     if target > line[0:end]:
+    #         file.close()
+    #         return None
+    pass
 
 print(func('a'))
